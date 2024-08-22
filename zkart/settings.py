13183376ef94665970697fd122532f168808bf77
muhamed-nasif <mohamednasifkarumarakkal@gmail.com>
2024-08-22 16:57:05 +0530

    signin correction & STATICFILES_DIRS

--- zkart/settings.py
+++ zkart/settings.py
@@ -182,10 +182,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-# STATICFILES_DIRS= [
-#     BASE_DIR/ 'static'
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS= [
+    BASE_DIR/ 'static'
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
